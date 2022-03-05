ddcf59fd3ef1e470ed25d8f305c00e9edfd40ee0
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-03-06 00:37:44 +0100

    initial commit

---
+++ src/main/java/com/ninni/mushfinders/Mushfinders.java
@@ -0,0 +1,10 @@
+package com.ninni.mushfinders;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mushfinders implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
